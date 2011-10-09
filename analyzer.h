157bf6339c5ed5513558522f0e9303228667a0d5
Udi Shamir <udishamir@gmail.com>
2011-10-09 15:24:49 +0200

    analyzer.h

---
+++ analyzer.h
@@ -0,0 +1,19 @@
+#define RELEASE "1.0"
+#define VER "camal_update/update.txt"
+#define HASH "md5.sum"
+#define UPDATE_DIR "/update"
+#define DEFAULT_PACK "camal_update.rar"
+#define CAMALDB "/etc/ver.db"
+#define EXZULU "/update"
+#define UNPACKMSG "/etc/.ans"
+#define FILECONF "camal_update/copy.conf"
+#define PACK "camal_update"
+
+#define DEFAULT_UPDATE_MSG "packer_detection, 32bit emulator, eset av"
+#define SHORT_OUT (uint32_t)20
+#define MD5MAX (uint32_t) 32
+#define MAX_OUT (uint32_t)150
+#define BIG_BUFFER (uint32_t)1024
+#define PASS (uint32_t)10
+#define MAX_RAR (uint32_t)47
+#define SSHORT (uint32_t)5
